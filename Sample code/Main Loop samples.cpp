8c27bd44b7ff1c38d1200b6c1d47d79de532ecab
Brian <xibriansonnenbergix@gmail.com>
2014-05-02 12:48:56 -0700

    converted tabs to spaces

--- Sample code/Main Loop samples.cpp
+++ Sample code/Main Loop samples.cpp
@@ -1,7 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <stdlib.h>
 
-//constants
+// Constants
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 #define INITIAL_CIRCLE_SIZE 100.f
@@ -9,21 +9,21 @@
 
 int main()
 {
-	// Initialize SFML
+    // Initialize SFML
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_WIDTH), "Flappy Bird");
-	window.setVerticalSyncEnabled(true);
+    window.setVerticalSyncEnabled(true);
     sf::CircleShape shape(INITIAL_CIRCLE_SIZE);
     shape.setFillColor(sf::Color::Green);
-	sf::Clock myTimer;
+    sf::Clock myTimer;
 
-	// Main loop
+    // Main loop
     while (window.isOpen())
     {
-		// Get how long it's been since the last frame was drawn
-		sf::Time frameTime = myTimer.getElapsedTime();
-		myTimer.restart();
+        // Get how long it's been since the last frame was drawn
+        sf::Time frameTime = myTimer.getElapsedTime();
+        myTimer.restart();
 
-		// Receive all incoming input
+        // Receive all incoming input
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -31,10 +31,10 @@ int main()
                 window.close();
         }
 
-		// Update game state
-		shape.setRadius(shape.getRadius() + frameTime.asMilliseconds() * CIRCLE_GROWTH_PER_MILLISECOND);
+        // Update game state
+        shape.setRadius(shape.getRadius() + frameTime.asMilliseconds() * CIRCLE_GROWTH_PER_MILLISECOND);
 
-		// Draw a frame and display it
+        // Draw a frame and display it
         window.clear();
         window.draw(shape);
         window.display();
